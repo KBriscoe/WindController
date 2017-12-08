e633b06a1503a35db50fd0b604c6fa4060f7ef08
KBriscoe <Kyler.briscoe@gmail.com>
2017-12-09 07:23:49 +0900

    Initial Commit

---
+++ controller.ino
@@ -0,0 +1,198 @@
+#include <bluefruit.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+#define PIN 30
+#define ANIMATION_PERIOD_MS  300
+
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(11, PIN, NEO_GRB + NEO_KHZ800);
+BLEUart bleuart;
+
+// Function prototypes for packetparser.cpp
+uint8_t readPacket (BLEUart *ble_uart, uint16_t timeout);
+float   parsefloat (uint8_t *buffer);
+void    printHex   (const uint8_t * data, const uint32_t numBytes);
+
+// Packet buffer
+extern uint8_t packetbuffer[];
+int mode = -1;
+int r = 255;
+int g = 0;
+int b = 0;
+
+//Settings
+int weather[9];
+int temperature[30];
+int humidity[15];
+int mode[4];
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit52 WIND Control"));
+  strip.begin();
+  strip.show();
+  Bluefruit.begin();
+  // Set max power. Accepted values are: -40, -30, -20, -16, -12, -8, -4, 0, 4
+  Bluefruit.setTxPower(4);
+  Bluefruit.setName("WIND Bracelet");
+
+  // Configure and start the BLE Uart service
+  bleuart.begin();
+  // Set up and start advertising
+  startAdv();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in Controller mode"));
+  Serial.println(F("Then activate/use the sensors, color picker, game controller, etc!"));
+  Serial.println();  
+}
+
+void startAdv(void)
+{
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  
+  // Include the BLE UART (AKA 'NUS') 128-bit UUID
+  Bluefruit.Advertising.addService(bleuart);
+
+  // Secondary Scan Response packet (optional)
+  // Since there is no room for 'Name' in Advertising packet
+  Bluefruit.ScanResponse.addName();
+
+  /* Start Advertising
+   * - Enable auto advertising if disconnected
+   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+   * - Timeout for fast mode is 30 seconds
+   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+   * 
+   * For recommended advertising interval
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
+   */
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
+}
+
+/**************************************************************************/
+/*!
+    @brief  Constantly poll for new command or response data
+*/
+/**************************************************************************/
+void loop() {
+  animatePixels(strip, r, g, b, ANIMATION_PERIOD_MS);
+  // Wait for new data to arrive
+  uint8_t len = readPacket(&bleuart, 500);
+  if (len == 0) return;
+
+  // Got a packet!
+  // printHex(packetbuffer, len);
+
+// Parse a color packet.
+  if (packetbuffer[1] == 'M') {
+    currentMode = Mode[packetbuffer[2].toInt()]
+    // Grab the RGB values from the packet and change the light color.
+    r = packetbuffer[2];
+    g = packetbuffer[3];
+    b = packetbuffer[4];
+    // Print out the color that was received too:
+    Serial.print ("RGB #");
+    if (r < 0x10) Serial.print("0");
+    Serial.print(r, HEX);
+    if (g < 0x10) Serial.print("0");
+    Serial.print(g, HEX);
+    if (b < 0x10) Serial.print("0");
+    Serial.println(b, HEX);
+  }else if (packetbuffer[1] == 'W') {
+    switch(packetbuffer[2]){
+      case 0: weather[0] = packetbuffer[3];
+              weather[1] = packetbuffer[4];
+              weather[2] = packetbuffer[5];
+              break;
+     case 1: weather[3] = packetbuffer[3];
+              weather[4] = packetbuffer[4];
+              weather[5] = packetbuffer[5];
+              break;
+     case 2: weather[6] = packetbuffer[3];
+              weather[7] = packetbuffer[4];
+              weather[8] = packetbuffer[5];
+              break;
+    }
+  }else if (packetbuffer[1] == 'H') {
+    
+  }else if (packetbuffer[1]== 'T') {
+    
+  }else if (packetbuffer[1] == 'M') {
+    //Change RGB Values
+  }
+  //Get Current Settings
+  
+}
+
+void changeColor(uint32_t c) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+  }
+  strip.show();
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void animatePixels(Adafruit_NeoPixel& strip, uint8_t r, uint8_t g, uint8_t b, int periodMS) {
+  int mode = millis()/periodMS % 2;
+  // Now light all the pixels and set odd and even pixels to different colors.
+  // By alternating the odd/even pixel colors they'll appear to march along the strip.
+  for (int i = 0; i < strip.numPixels(); ++i) {
+    if (i%2 == mode) {
+      strip.setPixelColor(i, r, g, b);  // Full bright color.
+    }
+    else {
+      strip.setPixelColor(i, r/4, g/4, b/4);  // Quarter intensity color.
+    }
+  }
+  strip.show();
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
